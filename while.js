6b51321e88ab7e97a499064016359502d63c7907
saisaran007 <sais26042@gmail.com>
2023-09-14 18:17:31 +0530

    while loops

---
+++ while.js
@@ -0,0 +1,79 @@
+let start = 1;
+
+let end = 50;
+
+
+while(start  <= end)
+ 
+    {
+   
+ console.log(start);
+  
+  start++;
+ 
+    }
+
+let start = 1;
+
+let end = 50;
+
+
+while(start  < end)
+ 
+    {
+    
+console.log(start);
+ 
+   start= start+2;
+  
+      }
+
+let start = 1;
+
+let end = 10;
+
+while(start  <= end)
+ 
+   {
+    
+console.log(3 * start);
+   
+ start++;
+   
+ 
+    }
+
+var start = 1;
+
+var end = 100;
+
+var sum = 0;
+
+var count = 0;
+
+
+while(start <= end)
+ 
+    {
+   
+ if(start % 2 == 0)
+   
+    {
+     
+ sum = sum + start;
+   
+   count++;
+     
+ console.log(sum + "-" + count);
+ 
+   }
+
+start ++;
+ 
+   }
+
+console.log(sum);
+
+console.log(count);
+
+console.log("Average + " +(sum/count));
