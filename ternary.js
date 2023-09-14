3fe32af7a9f65ca9061196afd79967ac0d615af4
saisaran007 <sais26042@gmail.com>
2023-09-14 18:11:40 +0530

    ternary operators

---
+++ ternary.js
@@ -0,0 +1,73 @@
+let DOB = 2005
+ 
+let age = 2023-2005
+
+
+if(age >=13 && age <=19)
+
+    {
+ 
+ console.log("Teenage");
+
+   }
+
+else if(age >=20 && age <=29)
+
+    {
+ console.log("Twenties");
+   
+}
+
+
+let str = "u";
+
+
+if(str == "a" || str == "e" || str == "i" || str == "o" || str == "u" )
+
+  {
+  
+console.log("Vowel");
+
+     }
+
+
+let str = "d";
+
+
+if(str != "a" || str != "e" || str != "i" || str != "o" || str != "u" )
+
+{
+ 
+ console.log("Consonants");
+
+}
+
+let a = 20;
+
+let b = 30;
+
+let c = 40;
+
+
+if(a > b && a > c)
+
+  {
+  
+console.log("a is the greatest")
+
+    }
+
+else if(b > a && b > c)
+  
+{
+  
+console.log("b is the greatest")
+ 
+}
+
+else
+    {
+ 
+ console.log("c is the greatest")
+ 
+}
